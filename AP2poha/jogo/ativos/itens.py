96fd93d04b6e7c2a8efe4404c3bc1fd3053f6600
GabrielMiler <142256207+GabrielMiler@users.noreply.github.com>
2023-11-21 22:54:17 -0300

    Add files via upload

---
+++ AP2poha/jogo/ativos/itens.py
@@ -0,0 +1,10 @@
+from Personagens.Player import status 
+from Personagens.Player import hp
+
+pocoes = {
+        "Poção de Vida 1" : int(hp * 0.20),
+        "Poção de Vida 2" : int(hp * 0.40),
+        "Poção de força" : int(status["fo"] + 1),
+        "Poção de força 2" : int(status["fo"] + 2),
+        "Poção de Defesa" : int(status["de"] + 1)
+    } 
